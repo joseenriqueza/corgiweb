4956a729282be837e8c4af105f68529ac8c1290b
Jose <joseaenriqueza@gmail.com>
2022-10-16 00:03:59 -0700

    test

--- src/tests/test_SetUp.py
+++ src/tests/test_SetUp.py
@@ -1,4 +1,6 @@
-import pytest  # TODO: read https://docs.pytest.org/en/6.2.x/fixture.html
+import pytest
+# TODO: read https://docs.pytest.org/en/6.2.x/fixture.html
+# TODO: read https://stackoverflow.com/questions/37353960/pytest-exits-with-no-error-but-with-collected-0-items
 from src import corgiweb as corgi
 import logging
 
@@ -8,6 +10,7 @@ LOGGER = logging.getLogger(__name__)
 
 def test_should_return_1():
     assert _SetUp.get_num_1() == 1
+
 
 def test_name_should_be_tony():
     assert _SetUp.name == "Tony"
