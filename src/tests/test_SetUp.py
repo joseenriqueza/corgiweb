1737c758057b23981ca201ad842caa237b0683f6
Jose <joseaenriqueza@gmail.com>
2022-10-16 00:12:08 -0700

    test

--- src/tests/test_SetUp.py
+++ src/tests/test_SetUp.py
@@ -1,7 +1,7 @@
 import pytest
 # TODO: read https://docs.pytest.org/en/6.2.x/fixture.html
 # TODO: read https://stackoverflow.com/questions/37353960/pytest-exits-with-no-error-but-with-collected-0-items
-from src import corgiweb as corgi
+import corgiweb as corgi
 import logging
 
 _SetUp = corgi.SetUp("Tony")
