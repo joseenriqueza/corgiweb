242afb1452376317cdd2c249383b86484809cfa1
Jose <joseaenriqueza@gmail.com>
2022-11-20 02:40:07 -0800

    test

--- tests/E2E/test_crawler_to_db.py
+++ tests/E2E/test_crawler_to_db.py
@@ -2,13 +2,14 @@ import corgiweb as corgi
 
 spiders = {}
 
-spiders["foxnews_com"] = corgi.crawler.crawler.Crawler(domain="foxnews.com",  # Website domain address
+
+spiders["foxnews_com"] = corgi.Crawler(domain="foxnews.com",  # Website domain address
                  user_agent= "*",  # User-Agent used to call the website
                  sitemap_url= "https://www.foxnews.com/sitemap.xml",  # Sitemap url
                  robots_url = "https://www.foxnews.com/robots.txt",  # Robots.txt url from the website
                  nodes_to_search= ["https://www.foxnews.com/politics"])  # JSON file with the URLS to search)
 
-spiders["walmart_com"] = corgi.crawler.crawler.Crawler(domain="walmart.com",  # Website domain address
+spiders["walmart_com"] = corgi.Crawler(domain="walmart.com",  # Website domain address
                  user_agent= "*",  # User-Agent used to call the website
                  robots_url = "https://www.walmart.com/robots.txt",  # Robots.txt url from the website
                  nodes_to_search= ["https://www.walmart.com/all-departments"])  # JSON file with the URLS to search)
@@ -31,7 +32,7 @@ for k, v in spiders.items():
     print("num_urls", len(v.new_urls))
 
 # Save the data on the custom partition
-data = corgi.data_and_queue.data_domain.DataAccess()
+data = corgi.DataAccess()
 data.start_django()
 for k, v in spiders.items():
     v.start_bfs()
